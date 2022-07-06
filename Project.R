ec1d1d921687c68b73a87b47cd34012ea26749d4
Janelle <janelle@Janelles-MacBook-Pro.local>
2022-07-06 21:22:12 +0800

    R script

---
+++ Project.R
@@ -0,0 +1,16 @@
+# Packages ----
+pacman::p_load(tidyverse, lubridate,
+               tidymodels,
+               skimr, GGally, ggstatsplot, Hmisc, jtools, huxtable, interactions,
+               usemodels, ranger, doParallel, vip,
+               DT, plotly,
+               ggthemes, scales, ggthemr, ggfortify, ggstance, ggalt,
+               broom, modelr,
+               shiny, shinydashboard
+               )
+
+# Load dataset
+
+# Actual work
+
+# Results??
