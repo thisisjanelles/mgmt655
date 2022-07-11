3c7a7c44980feb7e3d47ad0bdca30639f94e101e
Sanath Nadig <sanathmnadig@gmail.com>
2022-07-11 11:20:37 +0800

    committing file after import of dataset

--- Project.R
+++ Project.R
@@ -8,9 +8,17 @@ pacman::p_load(tidyverse, lubridate,
                broom, modelr,
                shiny, shinydashboard
                )
-
 # Download dataset
 # https://www.kaggle.com/datasets/prasertk/cities-with-the-best-worklife-balance-2022
+
+# Read dataset ----
+input_data <- read_csv("input_dataset.csv")
+
+input_data %>% group_by(Country) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
+
+skim(input_data)
 
 # Set dir path
 
