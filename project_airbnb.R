a79c1a50ee3047eacb9f328436651ca6e5996caf
Sanath Nadig <sanathmnadig@gmail.com>
2022-07-24 09:33:48 +0800

    First commit of new dataset

---
+++ project_airbnb.R
@@ -0,0 +1,638 @@
+pacman::p_load(tidyverse, lubridate,
+               tidymodels,
+               skimr, GGally, ggstatsplot, Hmisc, jtools, huxtable, interactions,
+               usemodels, ranger, doParallel, vip,
+               DT, plotly,
+               ggthemes, scales, ggthemr, ggfortify, ggstance, ggalt,
+               broom, modelr,
+               shiny, shinydashboard,
+               finetune, xgboost
+)
+
+# Read Dataset ----
+input_data <- read_csv("AB_NYC_2019.csv")
+skim(input_data)
+view(input_data)
+
+cleaned_data <- input_data %>% 
+  filter(price != 0) %>%
+  select(-name, -host_name, -host_id, -last_review) %>%
+  mutate(across(c(neighbourhood_group,
+                  room_type,
+                  neighbourhood), as.factor)) %>%
+  mutate(log10_price = log10(price)) %>%
+  select(-price) # %>%
+           # %>% try without getting rid of the NAs
+  # na.omit()
+
+#THE DATA IS NOW CLEEEEEAAAANNNN
+skim(cleaned_data)
+
+view(cleaned_data)
+
+
+# RECIPE FOR EDA----
+
+## Recipe for log10_price----
+recipe_eda <- 
+  recipe(formula = log10_price ~ .,
+         data = cleaned_data) %>% 
+  step_rm(id) %>%
+  step_other(neighbourhood, threshold = 0.05) %>%
+  step_impute_knn(reviews_per_month) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_dummy(all_nominal_predictors())
+
+## Baking for log10_price ----
+baked_eda <- 
+  recipe_eda %>% # plan 
+  prep() %>% # for calculation
+  bake(cleaned_data) 
+
+glimpse(baked_eda)
+
+## Baking for rank ----
+# baked_rank <-
+#   recipe_rank %>%
+#   prep() %>%
+#   bake(cleaned_data)
+
+# baked_rank
+
+# CORRELATION ----
+
+## Correlation for score ----
+
+corr_table <- baked_eda %>% 
+  as.matrix(.) %>%
+  rcorr(.) %>%
+  tidy(.) %>%
+  rename(var1 = column1,
+         var2 = column2,
+         CORR = estimate) %>%
+  mutate(absCORR = abs(CORR)) %>%
+  filter(var1 == "log10_price" | var2 == "log10_price") %>%
+  DT::datatable()  
+
+corr_table
+
+# Feature Engineering ----
+
+cleaned_data %>% 
+  ggplot(aes(x = `Inclusivity & Tolerance`, y = `TOTAL SCORE`)) +
+  geom_point(color = "dodgerblue",
+             alpha = 0.3) +
+  geom_smooth(method = "loess",
+              formula = y ~ x,
+              se = F,
+              color = "purple") +
+  geom_smooth(method = "lm",
+              formula = y ~ x,
+              se = F,
+              color = "green") +
+  geom_smooth(method = "lm",
+              formula = y ~ poly(x, degree = 2),
+              color = "tomato3") +
+  theme_bw()
+
+# SPLITTING ----
+
+set.seed(22062801)
+
+data_split <- 
+  cleaned_data %>% 
+  initial_split(prop = 0.80)
+
+data_split
+
+## CREATE TRAIN AND TEST SETS
+
+data_train <- # training(rent_split)
+  data_split %>% 
+  training() # 80%
+
+data_test <- 
+  data_split %>% 
+  testing() # 20%
+
+# Recipe for prediction ----
+## Recipe 1: Price with BoxCox inputs----
+recipe_price_box <- 
+  recipe(formula = `log10_price`~ .,
+         data = data_train) %>% 
+  step_rm(id) %>%
+  step_other(neighbourhood, threshold = 0.05) %>%
+  step_BoxCox(calculated_host_listings_count) %>%
+  step_impute_knn(reviews_per_month) %>%
+  step_normalize(all_numeric_predictors()) %>% 
+  step_dummy(all_nominal_predictors())
+
+
+baked_price_box <- 
+  recipe_price_box %>% # plan 
+  prep() %>% # for calculation
+  bake(data_train) 
+
+glimpse(baked_price_box)
+
+# CREATE MODELS ----
+## Random Forest ----
+
+rf_model <- 
+  rand_forest() %>%
+  set_args(trees = 1000, mtry = tune(), min_n = tune()) %>%
+  set_engine("ranger",
+             importance = "permutation") %>% 
+  set_mode("regression")
+
+
+## XG Boost ----
+
+XG_BOOST <- # extreme gradient boosting
+  boost_tree(trees = 500L,
+             mtry = tune(),
+             min_n = tune(),
+             tree_depth = tune(),
+             sample_size = tune(),
+             learn_rate = tune()
+  ) %>% 
+  set_engine("xgboost") %>% 
+  set_mode("regression")
+
+## Linear Regression
+ols_model <- linear_reg() %>%
+  set_engine("lm") %>%
+  set_mode("regression")
+
+# CREATE WORKFLOWS ----
+
+## Random Forest for price with box----
+workflow_rf_price_box <-
+  workflow() %>% 
+  add_recipe(recipe_price_box) %>% 
+  add_model(rf_model)
+
+## XG Boost for price with box ----
+workflow_xg_price_box <- 
+  workflow() %>% 
+  add_recipe(recipe_price_box) %>% 
+  add_model(XG_BOOST)
+
+## Linear regression for price with box ----
+workflow_ols_price_box <- 
+  workflow() %>% 
+  add_recipe(recipe_price_box) %>% 
+  add_model(ols_model)
+
+# CROSS VALIDATION ----
+
+## cross validation for Random Forest ----
+set.seed(22062802)
+
+cv_rf <- 
+  data_train %>% 
+  vfold_cv(v = 10)
+
+cv_rf
+
+## cross validation for XG BOOST ----
+
+### score ----
+set.seed(22201701)
+
+cv_xg <- 
+  data_train %>% 
+  vfold_cv(v = 10,
+           strata = log10_price)# put output variable name here
+
+cv_xg
+
+## cross validation for Linear regression ----
+set.seed(22062802)
+
+cv_ols <- 
+  data_train %>% 
+  vfold_cv(v = 10)
+
+cv_ols
+
+doParallel::registerDoParallel()
+
+# TUNING ----
+## Random Forest score tuning ----
+### price with box ----
+tuned_rf_price_box <-
+  workflow_rf_price_box %>% 
+  tune_grid(resamples = cv_rf,
+            grid = 3:10)
+
+## XG Boost tuning ----
+
+set.seed(22201702)
+
+grid_xg <-
+  grid_max_entropy(
+    mtry(c(5L, 10L),
+    ),
+    min_n(c(10L, 40L)
+    ),
+    tree_depth(c(5L, 10L)
+    ),
+    sample_prop(c(0.5, 1.0)
+    ),
+    learn_rate(c(-2, -1)
+    ),
+    size = 20
+  )
+
+### Price with box ----
+tuned_xg_price_box <- 
+  workflow_xg_price_box %>% 
+  tune_grid(resamples = cv_xg,
+            grid = grid_xg,
+            control = control_grid(save_pred = T)
+  )
+
+## OLS Tuning ----
+### Price with box ----
+tuned_ols_price_box <-
+  workflow_ols_price_box %>%
+  tune_grid(resamples = cv_ols,
+            grid = 3:10)
+
+
+# PARAMETERS AFTER TUNING ----
+
+## Random Forest parameters ----
+
+### Price with box ----
+parameters_tuned_rf_price_box <- 
+  tuned_rf_price_box %>% 
+  select_best(metric = "rmse")
+
+## XG Boost parameters ----
+
+### Price with box ----
+parameters_tuned_xg_price_box <-
+  tuned_xg_price_box %>%
+  select_best(metric = "rmse")
+
+## Linear regression parameters ----
+
+### Price with box ----
+parameters_tuned_ols_price_box <- 
+  tuned_ols_price_box %>% 
+  select_best(metric = "rmse")
+
+
+# FINALIZE WORKFLOW ----
+
+## Random Forest ----
+
+### Price with box ----
+finalized_workflow_rf_price_box <-
+  workflow_rf_price_box %>% 
+  finalize_workflow(parameters_tuned_rf_price_box)
+
+## XG Boost ----
+
+### Price with box ----
+finalized_workflow_xg_price_box <- 
+  workflow_xg_price_box %>% 
+  finalize_workflow(parameters_tuned_xg_price_box)
+
+## Linear regression ----
+
+### Price with box ----
+finalized_workflow_ols_price_box <-
+  workflow_ols_price_box %>% 
+  finalize_workflow(parameters_tuned_ols_price_box)
+
+# LAST FIT ----
+
+## Random Forest ----
+
+### Price with box ----
+fit_rf_price_box <-
+  finalized_workflow_rf_price_box %>% 
+  last_fit(data_split)
+
+## XG Boost ----
+
+### Price with box ----
+fit_xg_price_box <-
+  finalized_workflow_xg_price_box %>% 
+  last_fit(data_split)
+
+## Linear regression ----
+
+### Price with box ----
+fit_ols_price_box <-
+  finalized_workflow_ols_price_box %>% 
+  last_fit(data_split)
+
+# PERFORMANCE ----
+
+## Random Forest ----
+
+### Price with box ----
+performance_rf_price_box <- 
+  fit_rf_price_box %>% 
+  collect_metrics() %>% 
+  mutate(algorithm = "Random Forest for price with BoxCox")
+
+## XG Boost ----
+
+### Price with box ----
+performance_xg_price_box <- 
+  fit_xg_price_box %>% # for_performance(fit_xg) %>% 
+  collect_metrics() %>%
+  mutate(algorithm = "XG Boost for price with BoxCox")
+
+## Linear regression ----
+
+### Price with box ----
+performance_ols_price_box <- 
+  fit_ols_price_box %>% 
+  collect_metrics() %>% 
+  mutate(algorithm = "Linear regression for price with BoxCox")
+
+# COMPARE PERFORMANCE OF DIFFERENT ALGORITHMS AND RECIPES ----
+
+bind_rows(performance_rf_price_box,
+          performance_xg_price_box,
+          performance_ols_price_box) %>%
+  select(-.estimator,
+         -.config) %>% 
+  pivot_wider(names_from = .metric,
+              values_from = .estimate) %>% 
+  datatable() %>% 
+  formatRound(columns = c("rmse",
+                          "rsq"),
+              digits = 2)
+
+# CHECK PREDICTIONS ----
+## Random Forest ----
+### Score ----
+prediction_rf_score <- 
+  fit_rf_score %>% 
+  collect_predictions() %>%
+  mutate(algorithm = "Random Forest for price")
+### Rank ----
+prediction_rf_score_nc <- 
+  fit_rf_score_nc %>% 
+  collect_predictions() %>%
+  mutate(algorithm = "Random Forest for rank")
+
+## XG Boost ----
+### Score ----
+prediction_xg_score <-
+  fit_xg_score %>%
+  collect_predictions() %>%
+  mutate(algorithm = "XG Boost for price") 
+
+### Rank ----
+prediction_xg_score_nc <-
+  fit_xg_score_nc %>%
+  collect_predictions() %>%
+  mutate(algorithm = "XG Boost for rank")
+
+## Linear regression ----
+### Score ----
+prediction_ols_score <- 
+  fit_ols_score %>% 
+  collect_predictions() %>%
+  mutate(algorithm = "Linear regression for price")
+
+### Rank ----
+prediction_ols_score_nc <- 
+  fit_ols_score_nc %>% 
+  collect_predictions() %>%
+  mutate(algorithm = "Linear regression for rank")
+
+# CONSOLIDATE PREDICTIONS ----
+
+cleaned_data <- 
+  cleaned_data %>% 
+  tibble::rowid_to_column(".row")
+
+data_test_with_row <- data_test %>%
+  rename(.row = rowid)
+
+data_with_predictions <-
+  data_test_with_row %>%
+  inner_join(#prediction_rf_score %>% select(.row, .pred),
+    # prediction_rf_rank %>% select(.row, .pred),
+    # prediction_xg_score %>% select(.row, .pred),
+    # prediction_xg_rank %>% select(.row, .pred),
+    prediction_ols_score %>% select(.row, .pred),
+    # prediction_ols_rank %>% select(.row, .pred),
+    by = ".row")
+
+# TODO: Create a data frame comparing the outputs of all models with the original output.
+
+# CREATE PLOTS ----
+rf_score_plot <- 
+  prediction_rf_score %>% 
+  select(.row, log10_price, .pred)%>%
+  ggplot(aes(x = log10_price,
+             y = .pred)
+  ) + 
+  geom_point(color = "dodgerblue",
+             alpha = 0.50) + 
+  geom_abline(color = "red",
+              lty = 2) +
+  labs(y = "Predicted score of city",
+       x = "Actual score of city",
+       title = "Predicting city work-life balance score using RF") + 
+  theme_bw()
+
+rf_rank_plot <- 
+  prediction_rf_score_nc %>% 
+  select(.row, `TOTAL SCORE`, .pred) %>%
+  ggplot(aes(x = `TOTAL SCORE`,
+             y = .pred)
+  ) + 
+  geom_point(color = "dodgerblue",
+             alpha = 0.50) + 
+  geom_abline(color = "red",
+              lty = 2) +
+  labs(y = "Predicted rank of city",
+       x = "Actual rank of city",
+       title = "Predicting city work-life balance rank using RF") + 
+  theme_bw()
+
+xg_score_plot <- 
+  prediction_xg_score %>% 
+  select(.row, log10_price, .pred) %>%
+  ggplot(aes(x = log10_price,
+             y = .pred)
+  ) + 
+  geom_point(color = "dodgerblue",
+             alpha = 0.50) + 
+  geom_abline(color = "red",
+              lty = 2) +
+  labs(y = "Predicted score of city",
+       x = "Actual score of city",
+       title = "Predicting city work-life balance score using XG") + 
+  theme_bw()
+
+xg_rank_plot <- 
+  prediction_rf_score_nc %>% 
+  select(.row, `TOTAL SCORE`, .pred) %>%
+  ggplot(aes(x = `TOTAL SCORE`,
+             y = .pred)
+  ) + 
+  geom_point(color = "dodgerblue",
+             alpha = 0.50) + 
+  geom_abline(color = "red",
+              lty = 2) +
+  labs(y = "Predicted rank of city",
+       x = "Actual rank of city",
+       title = "Predicting city work-life balance rank using XG") + 
+  theme_bw()
+
+# grid.arrange(rf_score_plot, rf_rank_plot, 
+#              xg_score_plot, xg_rank_plot,
+#              ncol = 2)
+
+ols_score_plot <- 
+  prediction_ols_score %>% 
+  select(.row, log10_price, .pred) %>%
+  ggplot(aes(x = log10_price,
+             y = .pred)
+  ) + 
+  geom_point(color = "dodgerblue",
+             alpha = 0.50) + 
+  geom_abline(color = "red",
+              lty = 2) +
+  labs(y = "Predicted score of city",
+       x = "Actual score of city",
+       title = "Predicting city work-life balance score using OLS") + 
+  theme_bw()
+
+
+ols_rank_plot <- 
+  prediction_ols_score_nc %>% 
+  select(.row, `TOTAL SCORE`, .pred) %>%
+  ggplot(aes(x = `TOTAL SCORE`,
+             y = .pred)
+  ) + 
+  geom_point(color = "dodgerblue",
+             alpha = 0.50) + 
+  geom_abline(color = "red",
+              lty = 2) +
+  labs(y = "Predicted rank of city",
+       x = "Actual rank of city",
+       title = "Predicting city work-life balance rank using OLS") + 
+  theme_bw()
+
+grid.arrange(rf_score_plot, 
+             xg_score_plot,
+             ols_score_plot,
+             ncol = 1)
+
+# Results??
+
+
+
+# SHINY APP ----
+model <- 
+  readRDS("/Users/janelle/Development/mgmt655/xg_upsample.rds")
+
+model$pre$mold$predictors %>% 
+  colnames() %>% 
+  as_tibble()
+
+model_rf <- 
+  readRDS("/Users/janelle/Development/mgmt655/rf_rose.rds")
+
+model_rf$pre$mold$predictors %>% 
+  colnames() %>% 
+  as_tibble()
+
+# User Interface ----
+
+ui <- 
+  dashboardPage(skin = "purple", # Page
+                dashboardHeader(title = "City Work/Life Balance Score Prediction App",
+                                titleWidth = 320), # Header
+                dashboardSidebar( # Sidebar
+                  menuItem(
+                    "Work/Life Score Prediction App",
+                    tabName = "score_tab",
+                    icon = icon("city")
+                  )
+                ), 
+                dashboardBody(
+                  tabItem(
+                    tabName = "score_tab",
+                    # Box containing the prediction results
+                    box(valueBoxOutput("score_prediction") 
+                    ),
+                    # Sliders
+                    box(sliderInput("vacations",
+                                    label = "Minimum Vacations Offered (Days)",
+                                    min = 0.00,
+                                    max = 30.00,
+                                    value = 10.00)
+                    ),
+                    box(sliderInput("unemployment",
+                                    label = "Unemployment",
+                                    min = 50.00,
+                                    max = 100.00,
+                                    value = 94.00)
+                    )
+                  ) # Body
+                )
+  )
+
+# Server ----
+
+server <- function(input, output)
+{
+  output$score_prediction <- 
+    renderValueBox({
+      
+      prediction <- 
+        predict(
+          model_rf,
+          tibble(
+            "age" = input$age,
+            "avg_glucose_level" = input$avg_glucose_level,
+            #"bmi" = input$bmi
+          )
+        )
+      
+      prediction_prob <- 
+        predict(
+          model_rf,
+          tibble(
+            "vacations" = input_data$`Minimum Vacations Offered (Days)`,
+            "unemployment" = input_data$Unemployment,
+          ),
+          type = "prob"
+        ) %>% 
+        select(.pred_1)
+      
+      prediction_statement <- 
+        if_else(prediction$.pred_class == "1", 
+                "Yes", "No")
+      
+      prediction_visual <- 
+        if_else(prediction$.pred_class == "1",
+                "red", "green")
+      
+      valueBox(
+        value = paste0(round(prediction_prob$.pred_1*100, 1), "%"),
+        subtitle = paste0("Will this individual have stroke? ",
+                          prediction_statement),
+        color = prediction_visual
+      )
+      
+    })
+}
+
+# Run ----
+
+shinyApp(ui, server)
